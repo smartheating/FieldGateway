ab8b664446ea4f747e0cc1572fc733c3281059ad
Pascal Weiß <pa-weiss@gmx.de>
2019-06-14 18:57:16 +0200

    changed working dir

--- field_gateway/__main__.py
+++ field_gateway/__main__.py
@@ -1,14 +1,14 @@
 from os import environ
 from commons import read_device_ids
 import yaml
-from commons import get_project_root, store_device_ids
+from commons import store_device_ids
 from module_factory import ModuleFactory
 from threading import Event
 
 if __name__ == '__main__':
 
     # read the environment variables
-    env_config = environ.get('CONFIG', '../config.yaml')
+    env_config = environ.get('CONFIG', 'config.yaml')
 
     # load the config file
     with open(env_config, 'r') as f:
